05a6c77ba04e45229040ebbc7091f00f41d170d0
dfinke <finked@hotmail.com>
2019-10-14 11:11:04 -0400

    Update tests

--- __tests__/InvokePowerShellNotebook.tests.ps1
+++ __tests__/InvokePowerShellNotebook.tests.ps1
@@ -18,4 +18,25 @@ Describe "Test Invoke PS Notebook" {
         $actual[1] | Should Be 11
     }
 
+    It "Should have these results from the testPSExcel.ipynb" {
+        $actual = Invoke-PowerShellNotebook "$PSScriptRoot\GoodNotebooks\testPSExcel.ipynb"
+
+        $actual.Count | Should Be 3
+
+        # Returns results from get-service
+        $propertyNames = $actual[0][0].psobject.Properties.name
+        $propertyNames[0] | Should Be 'Status'
+        $propertyNames[1] | Should Be 'Name'
+        $propertyNames[2] | Should Be 'DisplayName'
+
+        # Returns results from get-process
+        $propertyNames = $actual[1][0].psobject.Properties.name
+        $propertyNames[0] | Should Be 'Company'
+        $propertyNames[1] | Should Be 'Name'
+        $propertyNames[2] | Should Be 'Handles'
+
+        # Returns results from an array
+        $actual[2].Count | Should Be 10
+    }
+
 }
