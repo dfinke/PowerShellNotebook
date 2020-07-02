e08d8ccd753aed363fb04823489a1689ffd12f81
dfinke <finked@hotmail.com>
2020-07-02 16:42:49 -0400

    Handles reading from a URL

--- ExportNotebookToPowerShellScript.ps1
+++ ExportNotebookToPowerShellScript.ps1
@@ -3,22 +3,35 @@ function Export-NotebookToPowerShellScript {
         .SYNOPSIS
         Exports all code blocks from a PowerShell Notebook to a PowerShell script
 
+        .DESCRIPTION
+        Exports from either a local notebook or one on the internet
+
         .Example
         Export-NotebookToPowerShellScript .\TestPS.ipynb
         Get-Content .\TestPS.ps1
-    #>
+
+        .Example
+        Export-NotebookToPowerShellScript "https://raw.githubusercontent.com/dfinke/PowerShellNotebook/AddJupyterNotebookMetaInfo/samplenotebook/powershell.ipynb"
+        Get-Content .\powershell.ps1
+        #>
     [CmdletBinding()]
     param(
         $FullName,
         $outPath = "./"
     )
+    Write-Progress -Activity "Exporting PowerShell Notebook" -Status $FullName
+    
+    if ([System.Uri]::IsWellFormedUriString($FullName, [System.UriKind]::Absolute)) {
+        $outFile = $FullName.split('/')[-1]
+    }
+    else {
+        $outFile = (Split-Path -Leaf $FullName)
+    }
+    
+    $outFile = $outFile -replace ".ipynb", ".ps1"
+    $fullOutFileName = $outPath + $outFile
 
-    if (Test-Path $FullName) {
-        Write-Progress -Activity "Exporting PowerShell Notebook" -Status $FullName
-        $outFile = (Split-Path -Leaf $FullName) -replace ".ipynb", ".ps1"
-        $fullOutFileName = $outPath + $outFile
-
-        $heading = @"
+    $heading = @"
 <#
     Created from: $($FullName)
 
@@ -27,19 +40,14 @@ function Export-NotebookToPowerShellScript {
 #>
 
 "@ 
-        $heading | Set-Content $fullOutFileName
 
-        $result = foreach ($sourceBlock in (Get-NotebookContent $FullName -JustCode).Source) {
-            $sourceBlock
-            ""
-        }
-
-        $result | Add-Content  $fullOutFileName
-        # (Get-NotebookContent $FullName -JustCode).Source | Add-Content  $fullOutFileName
-
-        Write-Verbose "$($outFile) created"
+    $heading | Set-Content $fullOutFileName    
+    $result = foreach ($sourceBlock in (Get-NotebookContent $FullName -JustCode).Source) {
+        $sourceBlock
+        ""
     }
-    else {
-        Write-Warning "File: $($FullName) not found"
-    }
+
+    $result | Add-Content  $fullOutFileName
+
+    Write-Verbose "$($outFile) created"
 }
