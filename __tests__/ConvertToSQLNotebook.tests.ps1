217feebb11454bbb0e8c85094a685af6704664db
Aditya Dakur Rudraiah <addakur@microsoft.com>
2020-07-29 18:10:59 -0700

    Add tests

--- __tests__/ConvertToSQLNotebook.tests.ps1
+++ __tests__/ConvertToSQLNotebook.tests.ps1
@@ -3,28 +3,34 @@ Import-Module $PSScriptRoot\..\SQLNotebook.psm1 -Force
 Describe "Test ConvertTo-SQLNoteBook" {
     It "Should convert the file to an ipynb" {
         $demoTextFile = "$PSScriptRoot\DemoFiles\demo.sql"
-        $fullName = "TestDrive:\sqlTestConverted.ipnyb"
+        $fullName = "C:\temp\sqlTestConverted.ipnyb"
 
-        ConvertTo-SQLNoteBook -InputFileName $demoTextFile -OutputNotebookName $fullName
-        { Test-Pat $fullName } | Should Be $true
+        try{
+            ConvertTo-SQLNoteBook -InputFileName $demoTextFile -OutputNotebookName $fullName
+            { Test-Pat $fullName } | Should Be $true
 
-        $actual = Get-NotebookContent -NoteBookFullName $fullName
-        $actual.Count | Should Be 11
+            $actual = Get-NotebookContent -NoteBookFullName $fullName
+            $actual.Count | Should Be 7
 
-        $actual = Get-NotebookContent -NoteBookFullName $fullName -JustCode
+            $actual = Get-NotebookContent -NoteBookFullName $fullName -JustCode
 
-        $actual.Count | Should Be 4
-        $actual[0].Source | Should BeExactly 'select DateDiff(MI,StartDate,EndDate) AS Timetaken,* FROM table1'
-        $actual[1].Source | Should BeExactly 'SELECT * FROM table2 WHERE id = 1'
-        $actual[2].Source | Should BeExactly 'SELECT * FROM table3 where ID = 7'
-        $actual[3].Source | Should BeExactly 'SELECT * FROM table4 where ID = 8'
+            $actual.Count | Should Be 3
+            write-verbose "tests $($actual[0].Source)" -Verbose
+            $actual[0].Source | Should -BeLike '*table1*'
+            $actual[1].Source | Should -BeLike '*table3*'
 
-        $actual = Get-NotebookContent -NoteBookFullName $fullName -JustMarkdown
+            $actual = Get-NotebookContent -NoteBookFullName $fullName -JustMarkdown
 
-        $actual.Count | Should Be 7
-        $actual[0].Source | Should BeExactly 'Test1'
-        $actual[1].Source | Should Contain 'Multiline test'
-        $actual[2].Source | Should BeExactly 'Test2'
-        $actual[3].Source | Should BeExactly 'Test3'
+            $actual.Count | Should Be 4
+            $actual[0].Source.Trim() | Should BeExactly 'Test1'
+            $actual[1].Source.Trim() | Should -BeLike '*Multiline test*'
+            $actual[2].Source.Trim() | Should BeExactly 'Test2'
+            $actual[3].Source.Trim() | Should BeExactly 'Test3'
+        }
+        catch [System.Management.Automation.RuntimeException]{
+            Write-Verbose "Runtime exception encountered" -Verbose
+            Write-Verbose $_ -Verbose
+            throw
+        }
     }
 }
--- __tests__/DemoFiles/demo.sql
+++ __tests__/DemoFiles/demo.sql
@@ -1,18 +1,11 @@
 select DateDiff(MI,StartDate,EndDate) AS Timetaken,* FROM table1
-
 SELECT * FROM table2 WHERE id = 1
-
 /* Test1 */
-
 /* Multiline test
 1
 2
 */
-
 /* Test2 */
-
 SELECT * FROM table3 where ID = 7
-
 /* Test3 */
-
 SELECT * FROM table4 where ID = 8
