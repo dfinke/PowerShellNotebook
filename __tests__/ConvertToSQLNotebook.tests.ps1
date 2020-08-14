09198e746f1607a4f58cec39e878d53d20aa6f55
Aaron Nelson <aaron.nelson@microsoft.com>
2020-08-14 09:57:50 -0400

    Added third ConvertToSqlNotebook test.

--- __tests__/ConvertToSQLNotebook.tests.ps1
+++ __tests__/ConvertToSQLNotebook.tests.ps1
@@ -43,7 +43,7 @@ SELECT * FROM table4 where ID = 8'
     }
     It "Should convert the file to an ipynb with 3 code and 6 text cells" {
         $demoTextFile = "$PSScriptRoot\DemoFiles\demo_w3GOs.sql"
-        $fullName = "C:\temp\sqlTestConverted_demo_w3GOs.ipynb"
+        $fullName = "TestDrive:\sqlTestConverted_demo_w3GOs.ipynb"
 
         try{
             ConvertTo-SQLNoteBook -InputFileName $demoTextFile -OutputNotebookName $fullName
@@ -68,6 +68,38 @@ SELECT * FROM table4 where ID = 8'
             $actual[3].Source.Trim() | Should BeExactly 'Test2'
             $actual[4].Source.Trim() | Should BeExactly 'GO'
             $actual[5].Source.Trim() | Should BeExactly 'Test3'
+        }
+        catch [System.Management.Automation.RuntimeException]{
+            Write-Verbose "Runtime exception encountered" -Verbose
+            Write-Verbose $_ -Verbose
+            throw
+        }
+    }
+    It "Should convert the file to an ipynb with 3 code and 6 text cells" {
+        $demoTextFile = "$PSScriptRoot\DemoFiles\AdventureWorksMultiStatementSBatch_NoGO2.sql"
+        $fullName = "TestDrive:\AdventureWorksMultiStatementSBatch_NoGO2.ipynb"
+
+        try{
+            ConvertTo-SQLNoteBook -InputFileName $demoTextFile -OutputNotebookName $fullName
+            { Test-Path $fullName } | Should -Be $true
+
+            $actual = Get-NotebookContent -NoteBookFullName $fullName
+            $actual.Count | Should -Be 7
+
+            $actual = Get-NotebookContent -NoteBookFullName $fullName -JustCode
+
+            $actual.Count | Should -Be 3
+            write-verbose "Code cells: $($actual.Count)" -Verbose
+            $actual[0].Source | Should -BeLike '*Person.PersonPhone*'
+            $actual[1].Source | Should -BeLike '*GETUTCDATE*'
+
+            $actual = Get-NotebookContent -NoteBookFullName $fullName -JustMarkdown
+
+            $actual.Count | Should -Be 4
+            $actual[0].Source.Trim() | Should -BeLike 'Look up user and phone number by last name*'
+            $actual[1].Source.Trim() | Should -BeLike '*-- Wait for 4 seconds total after go 2'
+            $actual[2].Source.Trim() | Should -BeLike '*note the Go 2 has been*'
+            $actual[3].Source.Trim() | Should -BeLike 'Commenting out until the field is added*'
         }
         catch [System.Management.Automation.RuntimeException]{
             Write-Verbose "Runtime exception encountered" -Verbose
