59aa40152843be5181ce77a95c227e629154daa6
Aaron Nelson <aaron.nelson@microsoft.com>
2020-08-18 15:38:18 -0400

    Trying to get test working with TestDrive:\

--- __tests__/SQLNotebooks/ExportNotebookToSqlScript.tests.ps1
+++ __tests__/SQLNotebooks/ExportNotebookToSqlScript.tests.ps1
@@ -1,32 +1,39 @@
 Import-Module $PSScriptRoot\..\..\PowerShellNotebook.psd1 -Force
 
 Describe "Test Export-NotebookToSqlScript" {
-    It "Should create SQL file with correct contents" -Skip {        
+    It "Should create SQL file with correct contents" {        
         $outPath = "TestDrive:\"
+        $SQLFile = Join-Path -Path $outPath -ChildPath "Simple_SELECTs.SQL"
+        
+        Write-Verbose "$($outPath)" -Verbose
+        Write-Verbose "$($PSScriptRoot)/Simple_SELECTs.ipynb" -Verbose
+        
+        Test-Path "$PSScriptRoot\Simple_SELECTs.ipynb" | Should -Be $true
         
         Export-NotebookToSqlScript -FullName "$PSScriptRoot\Simple_SELECTs.ipynb" -outPath $outPath
-        $SQLFile = "$outPath\Simple_SELECTs.SQL"
+
+        Write-Verbose "$($SQLFile)" -Verbose
         
-        Test-Path $SQLFile  | Should -Be $true
+        Test-Path $SQLFile | Should -Be $true
 
-        $contents = Get-Content $SQLFile
+        # $contents = Get-Content $SQLFile
         
-        $contents[0] | Should -BeExactly '/*'
-        $contents[1].StartsWith('    Created from:') | Should -Be $true        
-        $contents[3].StartsWith('    Created by:') | Should -Be $true        
-        $contents[4].StartsWith('    Created on:') | Should -Be $true
-        $contents[5] | Should -BeExactly '*/'
+        # $contents[0] | Should -BeExactly '/*'
+        # $contents[1].StartsWith('    Created from:') | Should -Be $true        
+        # $contents[3].StartsWith('    Created by:') | Should -Be $true        
+        # $contents[4].StartsWith('    Created on:') | Should -Be $true
+        # $contents[5] | Should -BeExactly '*/'
 
-        $contents[7] | Should -BeExactly '/*  First, find out how many databases are on this instance.  */'
-        $contents[8] | Should -BeExactly ''
-        $contents[9] | Should -BeExactly 'SELECT *'
-        $contents[10] | Should -BeExactly '  FROM sys.databases'
-        $contents[19] | Should -BeExactly 'SELECT SYSDATETIME()'
+        # $contents[7] | Should -BeExactly '/*  First, find out how many databases are on this instance.  */'
+        # $contents[8] | Should -BeExactly ''
+        # $contents[9] | Should -BeExactly 'SELECT *'
+        # $contents[10] | Should -BeExactly '  FROM sys.databases'
+        # $contents[19] | Should -BeExactly 'SELECT SYSDATETIME()'
 
-        Remove-Item $SQLFile -ErrorAction SilentlyContinue
+        #Remove-Item $SQLFile -ErrorAction SilentlyContinue
     }
 
-    It "Should export the ipynb from a URL to SQL" -Skip {
+    It "Should export the ipynb from a URL to SQL" {
         $url = "https://raw.githubusercontent.com/microsoft/tigertoolbox/master/BPCheck/BPCheck.ipynb"
 
         Export-NotebookToSqlScript -FullName $url
