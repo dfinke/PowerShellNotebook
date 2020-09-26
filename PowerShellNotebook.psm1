57711d3ed9834c32c0d4030689a2573a3d28108d
dfinke <finked@hotmail.com>
2020-09-26 06:20:52 -0400

    Adding Invoke-ExecuteNotebook

--- PowerShellNotebook.psm1
+++ PowerShellNotebook.psm1
@@ -1,10 +1,11 @@
 ﻿. $PSScriptRoot\ConvertFromNotebookToMarkdown.ps1
 . $PSScriptRoot\ConvertMarkdownToNoteBook.ps1
 . $PSScriptRoot\ConvertToPowerShellNoteBook.ps1
+. $PSScriptRoot\ConvertToSQLNoteBook.ps1
 . $PSScriptRoot\ExportNotebookToPowerShellScript.ps1
+. $PSScriptRoot\ExportNotebookToSqlScript.ps1
 . $PSScriptRoot\GetNotebook.ps1
 . $PSScriptRoot\GetNotebookContent.ps1
+. $PSScriptRoot\InvokeExecuteNotebook.ps1
 . $PSScriptRoot\InvokePowerShellNotebook.ps1
 . $PSScriptRoot\PowerShellNotebookDSL.ps1
-. $PSScriptRoot\ConvertToSQLNoteBook.ps1
-. $PSScriptRoot\ExportNotebookToSqlScript.ps1
