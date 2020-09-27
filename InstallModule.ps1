8cfc4d23f7dd892c6508085bb46a3635e3c1cf28
dfinke <finked@hotmail.com>
2020-09-27 13:12:21 -0400

    Updated where module is copied

--- InstallModule.ps1
+++ InstallModule.ps1
@@ -1,12 +1,12 @@
-param ($fullPath)
+$moduleName = '\PowerShellNotebook'
+$targets = @(
+    'C:\Program Files\WindowsPowerShell\Modules' + $moduleName # PS 5.1
+    'C:\Program Files\PowerShell\Modules' + $moduleName # PS 7+
+)
 
-if (-not $fullPath) {
-    $fullpath = $env:PSModulePath -split ":(?!\\)|;|," |
-        Where-Object {$_ -notlike ([System.Environment]::GetFolderPath("UserProfile")+"*") -and $_ -notlike "$pshome*"} |
-            Select-Object -First 1
-            $fullPath = Join-Path $fullPath -ChildPath "PowerShellNotebook"
+foreach ($fullPath in $targets) {
+    Robocopy . $fullPath /mir /XD .vscode .git CI __tests__ data mdHelp /XF appveyor.yml azure-pipelines.yml .gitattributes .gitignore filelist.txt install.ps1 InstallModule.ps1 PublishToGallery.ps1
 }
 
-Push-location $PSScriptRoot
-Robocopy . $fullPath /mir /XD .vscode .git CI __tests__ data mdHelp /XF appveyor.yml azure-pipelines.yml .gitattributes .gitignore filelist.txt install.ps1 InstallModule.ps1 PublishToGallery.ps1
-Pop-Location
+"copied to: "
+$targets
