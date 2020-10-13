977b173f345467d65f66fc87c4197ad4eb0ec086
dfinke <finked@hotmail.com>
2020-10-13 17:20:49 -0400

    Added Get-NotebookDisplayData. For charts rendered. Gets the JavaScript

---
+++ GetNotebookDisplayData.ps1
@@ -0,0 +1,16 @@
+function Get-NotebookDisplayData {
+    param($NoteBookFullName)
+
+    $result = Get-NotebookContent -NoteBookFullName $NoteBookFullName -PassThru
+
+    foreach ($cell in $result.cells) {
+        if ($cell.outputs.'output_type' -eq 'display_data') {
+            [PSCustomObject][Ordered]@{
+                Source      = -join $cell.source
+                DisplayData = -join $cell.outputs.data.'text/html'
+            }
+        }
+    }
+}
+
+    
