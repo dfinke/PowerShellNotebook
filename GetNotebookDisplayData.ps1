1da0ebb41973e90cca03fa1f0379e74d9d65c1a4
dfinke <finked@hotmail.com>
2020-10-13 17:26:13 -0400

    Change property name to Display

--- GetNotebookDisplayData.ps1
+++ GetNotebookDisplayData.ps1
@@ -6,8 +6,8 @@ function Get-NotebookDisplayData {
     foreach ($cell in $result.cells) {
         if ($cell.outputs.'output_type' -eq 'display_data') {
             [PSCustomObject][Ordered]@{
-                Source      = -join $cell.source
-                DisplayData = -join $cell.outputs.data.'text/html'
+                Source  = -join $cell.source
+                Display = -join $cell.outputs.data.'text/html'
             }
         }
     }
