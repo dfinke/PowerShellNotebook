00503650f270910cbabdbda5e491cbef589420c1
dfinke <finked@hotmail.com>
2021-02-25 21:15:02 -0500

    make easier

--- InstallModule.ps1
+++ InstallModule.ps1
@@ -1,20 +1,11 @@
-$moduleName = '\PowerShellNotebook'
-
-if (($PSVersionTable).PSVersion.Major -eq 5) {
-    $targets = @('C:\Program Files\WindowsPowerShell\Modules' + $moduleName) # PS 5
-}
-else {
-    $targets = @('C:\Program Files\PowerShell\Modules' + $moduleName) # PS 7   
-}
-
-# $targets = @(
-#     #'C:\Program Files\WindowsPowerShell\Modules' + $moduleName # PS 5.1
-#     'C:\Program Files\PowerShell\Modules' + $moduleName # PS 7+
-# )
-
-foreach ($fullPath in $targets) {
-    Robocopy . $fullPath /mir /XD .vscode .git CI __tests__ data mdHelp /XF appveyor.yml azure-pipelines.yml .gitattributes .gitignore filelist.txt install.ps1 InstallModule.ps1 PublishToGallery.ps1
+ 
+if (-not $fullPath) {
+    $fullpath = $env:PSModulePath -split ":(?!\\)|;|," |
+    Where-Object { $_ -notlike ([System.Environment]::GetFolderPath("UserProfile") + "*") -and $_ -notlike "$pshome*" } |
+    Select-Object -First 1
+    $fullPath = Join-Path $fullPath -ChildPath 'PowerShellNotebook'
 }
 
-"copied to: "
-$targets
+Push-location $PSScriptRoot
+Robocopy . $fullPath /mir /XD .vscode .git CI __tests__ data mdHelp /XF appveyor.yml azure-pipelines.yml .gitattributes .gitignore filelist.txt install.ps1 InstallModule.ps1
+Pop-Location
